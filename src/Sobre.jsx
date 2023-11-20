fff48390910e7b9a659cecd0572d4d7aeb05c735
Diego Ramos <diegoframos36@gmail.com>
2023-11-20 10:41:39 -0300

    atualização de pagina e correção de problemas

--- src/Sobre.jsx
+++ src/Sobre.jsx
@@ -3,87 +3,86 @@ import styles from './Sobre.module.css';
 import Head from './Head';
 const Sobre = () => {
   return (
-    <section className={styles.mainContent + ' animeLeft'}>
-      <div className={styles.preContent + ' animeLeft'}>
-        <img src="https://i.imgur.com/2iPFzOq.jpg" alt="" />
-        <p>
-          A gastronomia, ao longo dos anos, tem experimentado uma evolução
-          constante, e a revolução tecnológica emerge como um dos catalisadores
-          mais significativos dessa transformação. Este ensaio busca explorar
-          como a <span className={styles.destaque}>tecnologia</span> está
-          redefinindo a arte culinária, transformando cozinhas em{' '}
-          <span className={styles.destaque}>laboratórios de inovação</span> e
-          proporcionando experiências gastronômicas sem precedentes.
-          <br /> A nossa pizzaria trouxe consigo uma gama de utensilios que
-          elevaram a cozinha a um novo patamar de inovação. Desde{' '}
-          <span className={styles.destaque}>
-            fogões de indução de alta potência{' '}
-          </span>{' '}
-          até{' '}
-          <span className={styles.destaque}>fornos de convecção precisos</span>,
-          os chefs agora têm à disposição equipamentos que não apenas aceleram o
-          processo de preparo, mas também permitem um controle preciso da
-          temperatura e do tempo, garantindo resultados consistentes.
-          <br />
-          Nossos clientes tambem não ficam de fora, aqui na{' '}
-          <span className={styles.destaque}>Dom El Magnifico</span> prezamos
-          totalmente o você, com estudos de massas 100%{' '}
-          <span className={styles.destaque}>naturais</span> pensado
-          exclusivamente nos nossos fiéis clientes que estão sempre ajudando na
-          divulgação e na melhoria do nosso trabalho. Esperamos poder contar com
-          todos para a melhora do nosso estabelecimento.
-        </p>
+    <section className={styles.content}>
+      <div className={styles.firstContent}>
+        <h1>L&apos;inizio</h1>
+        <p></p>
       </div>
-      <div className={styles.cards + ' animeLeft'}>
-        <div className={styles.contents + ' animeLeft'}>
-          <div className={styles.background + ' animeLeft'}>
-            <img
-              src="https://raw.githubusercontent.com/DiegoRamos36/Pizzaria-Magnifico/63174490f689ce1ac356613221b3388df9132ea1/src/Img/queijo.svg"
-              alt=""
-            />
-            <p>
-              O queijo é um dos ingredientes mais importantes de uma pizza, e a
-              Pizzaria Dom El Magnifico não economiza no quesito qualidade. A
-              casa utiliza uma variedade de queijos italianos, como mussarela,
-              parmesão, gorgonzola e provolone. O queijo da Dom El Magnifico é
-              sempre bem derretido e saboroso. Ele é distribuído de forma
-              uniforme pela pizza, garantindo que cada fatia tenha o mesmo
-              sabor.
-            </p>
-          </div>
-        </div>
-        <div className={styles.contents + ' animeLeft'}>
-          <div className={styles.background + ' animeLeft'}>
-            <img
-              src="https://raw.githubusercontent.com/DiegoRamos36/Pizzaria-Magnifico/63174490f689ce1ac356613221b3388df9132ea1/src/Img/tomate.svg"
-              alt=""
-            />
-            <p>
-              O tomate da El Magnifico é sempre saboroso e fresco. Ele tem um
-              sabor doce e ácido, com uma textura suave e cremosa. O molho de
-              tomate é bem equilibrado, com o sabor do tomate sendo o destaque.
-              Na pizza Marguerita da Dom El Magnifico, o tomate é o grande
-              destaque. O molho de tomate é bem espesso e saboroso, e o tomate
-              fresco adiciona um toque de frescor e leveza à pizza.
-            </p>
-          </div>
-        </div>
-        <div className={styles.contents + ' animeLeft'}>
-          <div className={styles.background + ' animeLeft'}>
-            <img
-              src="https://raw.githubusercontent.com/DiegoRamos36/Pizzaria-Magnifico/63174490f689ce1ac356613221b3388df9132ea1/src/Img/alface.svg"
-              alt=""
-            />
-            <p>
-              O alface da Dom El Magnifico é sempre fresco e crocante. Ele tem
-              um sabor suave e neutro, que não interfere no sabor dos outros
-              ingredientes da pizza. Na pizza Portuguesa da Dom El Magnifico, o
-              alface é um dos ingredientes principais. O alface é usado como
-              base da pizza, e é coberto com molho de tomate, queijo e outros
-              ingredientes.
-            </p>
-          </div>
-        </div>
+      <div className={styles.secondContent}>
+        <h4>
+          Em meio às colinas exuberantes e à atmosfera vibrante de uma região do
+          Rio de Janeiro profundamente imersa na rica cultura italiana, surgiu a
+          visão do Dom El Magnífico. Influenciado pelos cenários pitorescos e
+          pelos costumes acolhedores. Os aromas tentadores da culinária italiana
+          se misturavam ao calor tropical, criando uma sinfonia gastronômica
+          única. As fachadas coloridas do Dom El Magnífico refletiam a alegria
+          contagiante das ruas cariocas, enquanto os murais artísticos contavam
+          histórias de uma fusão encantadora entre a tradição italiana e a
+          energia pulsante do Rio de Janeiro.
+        </h4>
+        <br />
+        <h4>
+          A decoração do restaurante refletia a sofisticação das ruas italianas,
+          com fachadas coloridas, murais artísticos e mesas adornadas com
+          toalhas de linho branco, transportando os clientes para um pedacinho
+          da Itália em pleno coração carioca. O cardápio do Dom El Magnífico era
+          uma celebração da diversidade da cozinha italiana, desde as clássicas
+          massas artesanais até os pratos contemporâneos que refletiam a fusão
+          de sabores entre as tradições italianas e os ingredientes locais
+          frescos.
+        </h4>
+        <br />
+        <h4>
+          Cada prato contava uma história, uma narrativa que misturava o passado
+          e o presente em uma experiência culinária única. Os habitantes locais,
+          bem como os turistas que buscavam uma experiência gastronômica
+          autêntica, abraçaram calorosamente o Dom El Magnífico. O restaurante
+          tornou-se um ponto de encontro para aqueles que buscavam não apenas
+          uma refeição deliciosa, mas também uma imersão na cultura italiana em
+          meio ao cenário tropical do Rio de Janeiro.
+        </h4>
+      </div>
+      <div className={styles.thirdContent}>
+        <h1>Obiettivo</h1>
+        <img
+          src="https://www.melhoresdestinos.com.br/wp-content/uploads/2022/11/pizza-capa-2022-04.jpg"
+          alt=""
+        />
+      </div>
+      <div className={styles.fourthContent}>
+        <h4>
+          Buscamos proporcionar não apenas uma experiência gastronômica
+          excepcional, mas também criar momentos memoráveis para todos que
+          escolhem a Dom El Magnifico. Acreditamos que a qualidade dos
+          ingredientes, o cuidado na preparação e a paixão pela culinária se
+          refletem no sorriso de nossos clientes.
+        </h4>
+        <br />
+        <h4>
+          Nossa equipe está comprometida em oferecer um atendimento caloroso e
+          amigável, garantindo que cada cliente se sinta bem-vindo e apreciado.
+          Valorizamos a diversidade de paladares e procuramos atender a uma
+          ampla gama de preferências, garantindo que haja algo especial para
+          todos em nosso cardápio.
+        </h4>
+        <br />
+        <h4>
+          Além disso, estamos sempre abertos a feedbacks e sugestões, pois
+          acreditamos que a constante busca pela excelência é fundamental.
+          Queremos aprimorar continuamente nossos serviços e pratos para superar
+          as expectativas de nossos clientes.
+        </h4>
+        <br />
+        <h4>
+          Agradecemos por escolher a Pizzaria Dom El Magnifico e esperamos ter a
+          oportunidade de encantar você com nossas deliciosas pizzas e
+          atendimento excepcional. Estamos ansiosos para continuar a
+          compartilhar momentos felizes e memoráveis com você e todos os nossos
+          clientes.
+        </h4>
+      </div>
+      <div className={styles.lastContent}>
+        <h1>Grazie!</h1>
       </div>
     </section>
   );
